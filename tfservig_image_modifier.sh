f3b07a98e5efe1ba3c636b1d34d80f5417b1b567
ivanCanaveral <canaveral.ivan@gmail.com>
2018-11-16 10:26:28 +0100

    kubernetes manifests now available

--- tfservig_image_modifier.sh
+++ tfservig_image_modifier.sh
@@ -6,6 +6,12 @@ docker cp $(pwd)/models/trex serving_base:/models/trex
 # --change , -c Apply Dockerfile instruction to the created image
 # --message , -m		Commit message
 # --author , -a		Author (e.g., “John Hannibal Smith hannibal@a-team.com”)
-docker commit --change "ENV MODEL_NAME simple_model" serving_base $(whoami)/trex_serving
+docker commit --change "ENV MODEL_NAME simple_model" serving_base ivancanaveral/trex:first
 docker kill serving_base
 docker rm serving_base
+# to tag manually:
+#   docker tag <image_id> yourhubusername/your_image:your_tag
+# to push your image:
+#   docker push yourhubusername/trex_serving
+# if you want to remove your local image:
+#   docker rmi trex_serving
